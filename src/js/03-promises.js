f16134dd344eeac7ec0f76c3786fe7ef7cd86de7
Oleksandr-N <hekp1984@gmail.com>
2022-07-24 15:30:51 +0300

    dd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+
 function createPromise(position, delay) {
+console.log(delay)
   const shouldResolve = Math.random() > 0.3;
+  const  promise = new Promise ((resolve,reject)=>{
   if (shouldResolve) {
-    // Fulfill
+    resolve( {position, delay} );
   } else {
-    // Reject
+    reject( position, delay );
   }
+  
+})
+promise.then(({position, delay})=>console.log(`🍿 Fulfilled promise ${position} in ${delay} ms`)).catch((position, delay)=>console.log(`🧨 Rejected promise ${position} in ${delay} ms`));
 }
+const ref = {
+  form:document.querySelector(".form"),
+}
+
+ref.form.addEventListener('submit', (e)=>{event.preventDefault();
+  const formData = new FormData(e.currentTarget)
+  const dataByForm = {}
+  formData.forEach((value,name)=>{
+    dataByForm[name]=Number(value)
+  
+  })
+  for (let i = 1; i<= dataByForm.amount; i+=1){
+    createPromise(i,dataByForm.delay)
+    dataByForm.delay+=dataByForm.step
+    
+  }
+
+})
